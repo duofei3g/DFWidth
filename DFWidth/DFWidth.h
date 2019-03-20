8130efef862dc2023a99fc1922779a2e5fd2f5ba
duofei <duofei3g@163.com>
2019-03-20 09:47:39 +0800

    sdf

--- DFWidth/DFWidth.h
+++ DFWidth/DFWidth.h
@@ -9,5 +9,5 @@
 #import <Foundation/Foundation.h>
 
 @interface DFWidth : NSObject
-
++(NSInteger)CompareBegin:(NSString *)begin end:(NSString *)end;
 @end
--- DFWidth/DFWidth.m
+++ DFWidth/DFWidth.m
@@ -12,6 +12,33 @@
 
 @implementation DFWidth
 
-
++(NSInteger)CompareBegin:(NSString *)begin end:(NSString *)end{
+    NSInteger num = 100;
+    NSDate *bTM;
+    NSDate *eTM;
+    if (begin.length==10&&end.length==10) {
+        bTM =  [NSDate date:[begin substringWithRange:NSMakeRange(0, 10)] WithFormat:@"yyyy-MM-dd"];
+        eTM =  [NSDate date:[end substringWithRange:NSMakeRange(0, 10)] WithFormat:@"yyyy-MM-dd"];
+    }else{
+        bTM =  [NSDate date:[begin substringWithRange:NSMakeRange(0, 13)] WithFormat:@"yyyy-MM-dd HH"];
+        eTM =  [NSDate date:[end substringWithRange:NSMakeRange(0, 13)] WithFormat:@"yyyy-MM-dd HH"];
+    }
+    
+    NSComparisonResult result = [bTM compare:eTM];
+    if (result==NSOrderedSame)
+    {
+        num = 0;
+    }else if (result==NSOrderedAscending)
+    {
+        num = 1;
+        
+    }else if (result==NSOrderedDescending)
+    {
+        num = 2;
+        
+    }
+    NSLog(@"num:%ld",num);
+    return num;
+}
 
 @end
